20981bdda520efb939ae239802a91052e25035df
DAP <dlrayjr89@gmail.com>
2020-10-11 19:49:55 +0000

    bisectional search added

--- src/algorithms/Searching_Algorithms/bisection_search.py
+++ src/algorithms/Searching_Algorithms/bisection_search.py
@@ -1,9 +1,26 @@
+'''
+Step 
+'''
+
 def bisection_Search(n, arr):
+    # starting index
     start = 0
+    #Last index in list
     stop = len(arr) - 1
-    mid = (start + stop) // 2
-    if n == arr[mid]:
-        return f"{n} found at index: {mid}"
+
+    while start <= stop:
+        #to get the middle mid = start + stop // 2
+        mid = (start + stop) // 2
+        if n == arr[mid]:
+            return f"{n} found at index: {mid}"
+        # n greater than value at mid
+        elif n > arr[mid]:
+            # set start value to mid + 1
+            start = mid + 1
+        else:
+            stop = mid - 1
+    print('Starting index', start)
+    print('Last index', stop)
     return f"{n} not found in list"
 
 def create_list(max_val):
@@ -13,6 +30,7 @@ def create_list(max_val):
     return arr
 
 
-l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-num_to_search = 5
-print(bisection_Search(num_to_search, l))
+
+l = create_list(10)
+for num in range(15):
+    print(bisection_Search(num, l))
---
+++ src/algorithms/Searching_Algorithms/recur_bisection.py
@@ -0,0 +1,9 @@
+def recur_bisection(arr):
+    pass
+
+def create_list(max_value):
+    arr = []
+
+    for num in range(1, max_value + 1):
+        arr.append(num)
+    return arr
